eea4eb9f5daa02bcbd089c150a2bd159b8cc7b0b
Dan-Sutton <dewasutton@gmail.com>
2023-08-30 22:18:57 +0100

    home page skeleton setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,151 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import bandImage from "../../public/band-header-image.png";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <div>
+      <section className={styles.headerTitle}>
+        <h1>The Wildfire</h1>
+        <img className={styles.bandImage} src={bandImage.src} alt="" />
+      </section>
+
+      <section className={styles.about}>
+        <h2>ELEVATE YOUR EVENT</h2>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          The Wildfire are a fresh and energetic function/party band bringing a
+          dynamic blend of genres, from timeless classics to modern hits,
+          ensuring that every guest finds their groove on the dance floor. We
+          provide a professional and unforgettable experience, so that your is
+          event is not one to forget.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </section>
+
+      <section className={styles.packages}>
+        <h2>CATERING TO YOUR NEEDS</h2>
+        <p>
+          We provide a number of packages to cater to your specific needs on the
+          day.
+        </p>
+
+        <div className={styles.cards}>
+          <span className={styles.packageCard}>
+            <h3>BAND + BACKGROUND MUSIC</h3>
+            <p>
+              We’ll play our two live sets, and also provide music between sets.
+            </p>
+          </span>
+
+          <span className={styles.packageCard}>
+            <h3>BAND + DJ</h3>
+            <p>
+              We’ll play our two live sets, and also provide a DJ who can supply
+              music through the rest of the event.
+            </p>
+          </span>
+
+          <span className={styles.packageCard}>
+            <h3>ACOUSTIC BAND SETUP</h3>
+            <p>
+              We can provide a more stripped back band setup, if that’s your
+              preference.
+            </p>
+          </span>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <div className={styles.packageMessage}>
+          <h2>HAD SOMETHING ELSE IN MIND?</h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            <b>CONTACT</b> us and we’re sure we can provide a super solution!
           </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </section>
+
+      <section className={styles.about}>
+        <h2>WHAT ARE PEOPLE SAYING?</h2>
+        <div className={styles.quotes}>
+          <div className={styles.quote}>
+            <h3 className={styles.quoteTitle}>
+              “There aren’t enough words to say how fabulous The Wildfire were
+              at our Tipi wedding celebrations.
+            </h3>
+            <p>
+              As soon as we saw them play live locally, a few months beforehand,
+              we knew they were the ones for us and supporting local talent was
+              so important to us too. They literally rocked the tipi and the
+              forest, had everyone up on their feet and were simply brilliant.
+              True professionalism, great song covers, great lads. You certainly
+              have a lot more fans now. Thank you so much!“
+            </p>
+            <h4>Nicky Conway</h4>
+            <p>★★★★★</p>
+          </div>
+
+          <div className={styles.quote}>
+            <h3 className={styles.quoteTitle}>
+              “I cannot thank The Wildfire enough for playing at our wedding
+              this week.
+            </h3>
+            <p>
+              They played two sets to cater for the day and the evening - both
+              of which were absolutely amazing! So many people commented on how
+              talented they were and how fun their music was. People were up
+              dancing until the very end! They also got the groom, best man, and
+              friends up to sing along too. They were the highlight of the
+              wedding and I can't thank them enough for making our day so
+              special.“
+            </p>
+            <h4>Grace Christine</h4>
+            <p>★★★★★</p>
+          </div>
+
+          <div className={styles.quote}>
+            <h3 className={styles.quoteTitle}>“Wow what a band!</h3>
+            <p>
+              The Wildfire play a great variety of songs that never fail to get
+              people up and dancing The boys are so talented and are definitely
+              a must see. Can’t wait to have them back very soon… can’t
+              recommend them enough!“
+            </p>
+            <h4>Royal Fountain Inn</h4>
+            <p>★★★★★</p>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.contact}>
+        <h2>CONTACT</h2>
+        <div className={styles.contactInput}>
+          <div>
+            <input></input>
+            <input></input>
+          </div>
+          <div>
+            <input></input>
+            <input></input>
+          </div>
+          <div>
+            <select name="packages">
+              <option value="notSure">Not Sure</option>
+              <option value="band">Band + Background Music</option>
+              <option value="dj">Band + DJ</option>
+              <option value="acoustic">Acoustic Band Setup</option>
+            </select>
+            <input></input>
+          </div>
+
+          <textarea></textarea>
+        </div>
+
+        <p>
+          <i>Inputs marked with * are required</i>
+        </p>
+
+        <p>
+          <b>VIEW</b> our <b>FAQs</b> just in case any of your questions are
+          answered there!
+        </p>
+
+        <button>SUBMIT</button>
+      </section>
+    </div>
+  );
 }
